3de994df9a717680134274c9630d5c6a449e29dd
daheige <zhuwei313@hotmail.com>
2022-03-24 22:24:52 +0800

    feat: redis cmd use

--- part3/redis-demo/readme.md
+++ part3/redis-demo/readme.md
@@ -3,3 +3,6 @@
 redis-rs基本操作
 
 https://github.com/mitsuhiko/redis-rs
+
+# redis cmd手册
+http://redisdoc.com/
--- part3/redis-demo/src/main.rs
+++ part3/redis-demo/src/main.rs
@@ -1,24 +1,62 @@
+use std::collections::HashMap;
+use std::ptr::hash;
 use redis::{self, Commands};
 
+// redis cmd 基本操作
 fn main() {
     println!("redis-demo...");
     let mut redis_obj = RedisService::new("redis://:@127.0.0.1:6379/0");
-    let my_value = redis_obj.fetch_an_integer().unwrap();
-    println!("my_value:{}", my_value);
 
-    let my_name = "my_name1";
-    let name = redis_obj.fetch_cmd_res(my_name.to_string()).unwrap();
-    println!("name:{}", name);
+    // 大部分命令返回值是一个 redis::RedisResult<()>，具体可以点击看源码
+    // 执行set_ex
+    let _: redis::RedisResult<()> = redis_obj.conn.set_ex("user", "heige", 120);
 
-    // 另一种方式
-    let name = redis_obj.fetch_cmd_res(my_name.to_string());
-    if let Ok(name) = name {
-        println!("other name:{}", name);
-    } else {
-        println!("not found key:{}", my_name);
-    }
+    println!("get value");
+    // let res : redis::RedisResult<String> = redis_obj.conn.get("user");
+    // println!("res: {}",res.unwrap());
 
-    let _ = redis_obj.do_some_cmd();
+    // 返回值是 pub type RedisResult<T> = Result<T, RedisError>
+    let user: String = redis_obj.conn.get("user").unwrap();
+    println!("user: {}", user);
+
+    // let _ : redis::RedisResult<i64> = redis_obj.conn.incr("my_number",1);
+    let num: i64 = redis_obj.conn.incr("my_number", 1).unwrap();
+    println!("num:{}", num);
+
+    // hash set
+    let _: redis::RedisResult<()> = redis_obj.conn.hset("my_hash", "a", "abc");
+    let _: redis::RedisResult<()> = redis_obj.conn.hset("my_hash", "b", "efg");
+
+    // hash get
+    let a: String = redis_obj.conn.hget("my_hash", "a").unwrap_or("".to_string());
+    println!("a: {}", a);
+
+    // hash hgetall 哈希获得所有数据 map
+    let hash: HashMap<String, String> = redis_obj.conn.hgetall("my_hash").unwrap();
+    println!("hash: {:?}", hash);
+    println!("hash.a = {}", hash.get("a").unwrap());
+
+    let len: i64 = redis_obj.conn.hlen("my_hash").unwrap_or(0); // 如果不存在就初始化长度为0
+    println!("len = {}", len);
+
+    // set myname dahege ex 100 nx
+    let _: redis::RedisResult<()> = redis::cmd("set").arg("my_name").arg("daheige").
+        arg("ex").arg(100).arg("nx").query(&mut redis_obj.conn);
+
+    // 一次设置多个值
+    let _: redis::RedisResult<()> = redis_obj.conn.set_multiple(&[("num1", 1), ("num2", 2)]);
+
+    // 一次设置多个hash key field value
+    let s = vec![("user1", "heige"), ("user2", "daheige")];
+    let _: redis::RedisResult<()> = redis_obj.conn.hset_multiple("my_hash", &s);
+
+    // hincy my_hash_number art:1:read_count 1 哈希计数器
+    // hget my_hash_number art:1:read_count
+    let _: redis::RedisResult<()> = redis_obj.conn.hincr("my_hash_number", "art:1:read_count", 1);
+
+    // 设置集合元素
+    let _: redis::RedisResult<()> = redis_obj.conn.sadd("my_set", 1);
+    let _: redis::RedisResult<()> = redis_obj.conn.sadd("my_set", 2);
 }
 
 struct RedisService {
@@ -68,5 +106,30 @@ impl RedisService {
         self.conn.set_ex("my_user", "heige", 120)?;
         self.conn.set_ex("user1", "daheige", 100)?;
         Ok(())
+    }
+}
+
+// redis 测试用例
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn redis_demo() {
+        let mut redis_obj = super::RedisService::new("redis://:@127.0.0.1:6379/0");
+        let my_value = redis_obj.fetch_an_integer().unwrap();
+        println!("my_value:{}", my_value);
+
+        let my_name = "my_name1";
+        let name = redis_obj.fetch_cmd_res(my_name.to_string()).unwrap();
+        println!("name:{}", name);
+
+        // 另一种方式
+        let name = redis_obj.fetch_cmd_res(my_name.to_string());
+        if let Ok(name) = name {
+            println!("other name:{}", name);
+        } else {
+            println!("not found key:{}", my_name);
+        }
+
+        let _ = redis_obj.do_some_cmd();
     }
 }
