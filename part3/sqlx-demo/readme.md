af54f41afa116002b03d14bfa6bffb2d7294d0e5
daheige <zhuwei313@hotmail.com>
2022-04-12 23:19:51 +0800

    feat: sqlx

---
+++ part3/sqlx-demo/readme.md
@@ -0,0 +1,12 @@
+# sqlx用法
+    https://github.com/launchbadge/sqlx
+    
+    sqlx = { version = "0.5", features = [ "runtime-tokio-rustls" , "mysql" ] }
+    tokio = { version = "1", features = ["full"] }
+    or
+    sqlx = { version = "0.5", features = [ "runtime-tokio-native-tls" , "mysql" ] }
+    tokio = { version = "1", features = ["full"] }
+
+# crate
+https://crates.io/crates/sqlx
+
---
+++ part3/sqlx-demo/src/main.rs
@@ -0,0 +1,78 @@
+use chrono::prelude::*;
+use futures::TryStreamExt;
+use sqlx;
+use sqlx::mysql::MySqlPoolOptions;
+use sqlx::Row;
+use std::env;
+use std::time::Duration;
+use tokio;
+
+#[derive(Debug)]
+struct Stu {
+    id: i64,
+    name: String,
+    age: i32,
+    id_card: String,
+    last_update: NaiveDate, // 时间格式
+}
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let default_dsn = "mysql://root:root1234@localhost/test";
+    let dsn = env::var("DB_DSN").unwrap_or(default_dsn.to_string());
+    let pool = MySqlPoolOptions::new()
+        .max_connections(50) // 最大连接数
+        .min_connections(3) // 最小连接数
+        .max_lifetime(Duration::from_secs(1800)) // 最大生命周期
+        .idle_timeout(Duration::from_secs(600)) // 空闲连接的生命周期
+        .connect_timeout(Duration::from_secs(10)) // 连接超时
+        .connect(&dsn)
+        .await?;
+
+    // 通过mysql ?占位符，bind 绑定参数形式查询
+    let row: (i64,) = sqlx::query_as("select ?")
+        .bind(120i64)
+        .fetch_one(&pool)
+        .await?;
+
+    println!("res: {}", row.0);
+    assert_eq!(row.0, 120);
+
+    // 1、使用fetch，获取cursor游标，自己处理row
+    let sql = "select * from student where id >= ?";
+    let mut rows = sqlx::query(sql).bind(1).fetch(&pool);
+    while let Some(row) = rows.try_next().await? {
+        let stu = Stu {
+            id: row.get("id"),
+            name: row.get("name"),
+            age: row.get("age"),
+            id_card: row.get("id_card"),
+            last_update: row.get("last_update"),
+        };
+
+        println!("stu = {:?}", stu);
+    }
+
+    // 2、使用fetch，加map，自动处理row
+    let sql = "select * from student where id >= ?";
+    let records = sqlx::query(sql)
+        .bind(1)
+        .map(|row: sqlx::mysql::MySqlRow| Stu {
+            id: row.get("id"),
+            name: row.get("name"),
+            age: row.get("age"),
+            id_card: row.get("id_card"),
+            last_update: row.get("last_update"),
+        })
+        .fetch(&pool);
+
+    tokio::pin!(records);
+
+    while let Some(s) = records.try_next().await? {
+        println!("s = {:?}", s);
+    }
+
+    // 3、使用execute，执行更新操作，返回affect_rows todo
+
+    Ok(())
+}
