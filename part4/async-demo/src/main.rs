5311ab0566ecc568dd723c6d15b8f72272d651e1
daheige <zhuwei313@hotmail.com>
2022-05-28 15:57:39 +0800

    feat: rust async/.await

--- part4/async-demo/src/main.rs
+++ part4/async-demo/src/main.rs
@@ -1,4 +1,53 @@
+// `block_on` blocks the current thread until the provided future has run to
+// completion. Other executors provide more complex behavior, like scheudling
+// multiple futures onto the same thread.
+//
+// block_on 会阻塞当前线程执行，直到提供的future执行完毕为止
+// 其他的执行器 executors 提供更加复杂的行为，比如说多个futures调度在同一个线程上
+use futures;
+use futures::executor::block_on;
+
+// async fn 返回一个Future ，需要在执行的时候才能起到作用
+async fn do_task() {
+    println!("do async task");
+}
+
+async fn learn_song() {
+    println!("learn song");
+}
+
+async fn sing_song(name: String) {
+    println!("sing song:{}", name);
+}
+
+async fn dance() {
+    println!("dance...")
+}
+
+async fn learn_and_sing() {
+    // 这里用.await 不是用block_on ,这样可以让其他future执行
+    let _ = learn_song().await;
+    sing_song("hello".to_string()).await;
+}
+
+// 返回返回一个执行器
+async fn do_more_tasks() {
+    let f1 = learn_and_sing();
+    let f2 = dance();
+    // 下面的join! 类似于.await ,但他可以等待多个 future 并发完成
+    // 如果说f1有了短暂的阻塞，这个时候f2 就会接管当前的线程
+    // 如果f2执行时候发生了阻塞，f1就会接管执行
+    futures::join!(f1, f2);
+}
+
 fn main() {
-    println!("Hello, world!");
-    println!("abc");
+    println!("start exec async future");
+    let future = do_task();
+
+    println!("task start");
+    block_on(future);
+    println!("task has done");
+
+    // async/.await 执行操作
+    block_on(do_more_tasks());
 }
--- part4/readme.md
+++ part4/readme.md
@@ -1,2 +1,27 @@
 # async 异步编程
 https://learnku.com/docs/async-book/2018
+
+# async/await
+    async/.await是rust语言用于编写同步代码像异步代码一样的内置工具
+    async将一个代码块转换为一个实现了 Future trait 的状态机。
+    虽然在同步的方法中调用阻塞函数会阻塞整个线程，但阻塞的 Futures 将让出线程所有权,
+    允许其他的 Futures 运行。
+
+# Future 原理
+```rust
+trait SimpleFuture {
+        type Output;
+        fn poll(&mut self, wake: fn()) -> Poll<Self::Output>;
+}
+
+// Poll 是一个状态机，要么完成，要么尚未完成
+// 通过调用该 poll 函数可以推进 Futures，这将推动 future 尽可能地完成。
+// 如果 Future 完成，它将返回 Poll::Ready(result)。
+// 如果 Future 尚未完成，它将返回 Poll::Pending 并安排在 Future 
+// 准备好进行更多进展时调用 wake() 函数。当 wake() 被调用时，
+// executor(执行者) 驱动 Future 将再次调用 poll，以便 Future 能够取得更多进展。
+enum Poll<T> {
+    Ready(T),
+    Pending,
+}
+```
