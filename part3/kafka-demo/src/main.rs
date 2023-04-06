fc82f86a95d8410d211f712fb34c4ad3083b4f30
daheige <zhuwei313@hotmail.com>
2023-04-06 23:30:35 +0800

    feat: kafka

---
+++ part3/kafka-demo/src/main.rs
@@ -0,0 +1,43 @@
+use env_logger;
+use kafka::error::Error as KafkaError;
+use kafka::producer::{Producer, Record, RequiredAcks};
+use std::time::Duration;
+fn main() {
+    env_logger::init(); // 初始化logger配置
+    println!("kafka demo....");
+    let broker = "localhost:9092";
+    let topic = "my-topic";
+    let data = "hello,rust".as_bytes();
+
+    if let Err(e) = message_publish(data, topic, vec![broker.to_owned()]) {
+        println!("failed producing message error:{}", e);
+    }
+}
+
+// send message
+fn message_publish<'a, 'b>(
+    data: &'a [u8],
+    topic: &'b str,
+    brokers: Vec<String>,
+) -> Result<(), KafkaError> {
+    println!("publish message at {:?} to {}", brokers, topic);
+
+    // create producer
+    let mut producer = Producer::from_hosts(brokers)
+        .with_ack_timeout(Duration::from_secs(1))
+        .with_required_acks(RequiredAcks::One)
+        .create()?;
+
+    // send message
+    producer.send(&Record {
+        topic,
+        partition: -1,
+        key: (),
+        value: data,
+    })?;
+
+    // you can send message by this way
+    producer.send(&Record::from_value(topic, data))?;
+
+    Ok(())
+}
